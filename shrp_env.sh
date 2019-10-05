543d42a3412a379b73044c1596b277ede2ece4fe
epicX67 <46811948+epicX67@users.noreply.github.com>
2019-10-05 22:07:49 +0530

    Create shrp_env.sh

---
+++ shrp_env.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+export SHRP_MAINTAINER="epicX"
+export SHRP_DEVICE="X00T"
+export SHRP_EDL_MODE="1"
+export SHRP_EXTERNAL="/external_sd"
+export SHRP_INTERNAL="/sdcard"
+export SHRP_OTG="/usb_otg"
+export SHRP_FLASH="1"
+export SHRP_FLASH_MAX_BRIGHTNESS="500"
+export SHRP_FONP_1="/sys/class/leds/led:torch_0/brightness"
+export SHRP_FONP_2="/sys/class/leds/led:torch_1/brightness"
+export SHRP_FONP_3="/sys/class/leds/led:switch_0/brightness"
+export SHRP_REC="/dev/block/bootdevice/by-name/recovery"
